cdf67878f5422c383b7bd9ef0c17338e67a98814
aos82 <94140638+aos82@users.noreply.github.com>
2021-12-03 18:44:00 -0700

    Added some time measurement to get speed data

--- Simple_Client_Files.py
+++ Simple_Client_Files.py
@@ -9,27 +9,31 @@ Created on Thu Nov 11 15:48:09 2021
 import socket
 import tqdm
 import os
+import time
+
 space = "<space>"
 buffer_size = 1024
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-filename = "GPS_Data.txt"
+filename = "20131124_144634.mp4"
 filesize = os.path.getsize(filename) 
-print(int(filesize))
 
 #creates socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT)) #Connects to the server    
 s.send(f"{filename}{space}{filesize}".encode())
 
-bar = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=512)
+
+bar = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=256, colour ='#00ff00')
 with open(filename, "rb") as f:
+
     while True:
         data = f.read(buffer_size)
         if not data:
             break
+        
         s.sendall(data)
         bar.update(len(data))
 s.close()
--- Simple_Server_Files.py
+++ Simple_Server_Files.py
@@ -8,6 +8,8 @@ Created on Thu Nov 11 13:03:56 2021
 import socket
 import tqdm
 import os
+import time
+
 
 space = "<space>"
 buffer_size = 1024
@@ -25,12 +27,14 @@ print("listening via", (HOST, PORT))
 conn, addr = s.accept() #accept() blocks and waits for an incoming connection
 print('Connected by', addr)
 
+start = time.time()
+
 recv = conn.recv(buffer_size).decode()
 filename, filesize = recv.split(space)
 filename = os.path.basename(filename)
 filesize = int(filesize)
 
-bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=256)
+bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=256, colour='#00ff00') # 256 = MB
 with open(filename, "wb") as f:
     while True: #infinite while loop
         data_read = conn.recv(buffer_size) #reads whatever data the client sends and echoes it back using conn.sendall() (1024 bytes) buffer size
@@ -38,6 +42,14 @@ with open(filename, "wb") as f:
             break
         f.write(data_read)
         bar.update(len(data_read))
-
 conn.close()        
 s.close()
+
+end = time.time()
+
+time = end-start
+
+xfr_rate = filesize / time
+
+
+print('\n\nTime = {:.2}s \nFile size = {:.2} MB \nTransfer rate = {:.3} MB/s\n'.format(time, filesize/(1024*1024), xfr_rate/(1024*1024)))
