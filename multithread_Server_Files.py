3df899736c9e254b34dcc07f0841cf9d00a4424c
aos82 <94140638+aos82@users.noreply.github.com>
2022-04-07 18:45:55 -0600

    latest as of 4/7/22

--- multithread_Server_Files.py
+++ multithread_Server_Files.py
@@ -1,60 +1,70 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 11 13:03:56 2021
-
-@author: aos82
-"""
-
 import socket
-import tqdm
 import os
-import time
-from _thread import *
+import threading
+import tqdm
+import os.path
 
-space = "<space>"
+
 buffer_size = 1024
-HOST = '0.0.0.0'  # Standard loopback interface address (localhost) can be a hostname, IP address, or empty string
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 65432      # Port to listen on (non-privileged ports are > 1023) should be an integer from 1-65535 (0 is reserved).
-Threadnumb = 0
+FORM = "utf-8"
+SERV_PATH = "RCVD_FILES"
+ADDR = (HOST, PORT)
+space = "<space>"
 
-#socket.socket creates a socket object that supports the context manager type
-#socket type. AF_INET is the Internet address family for IPv4
-#SOCK_STREAM is the socket type for TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    s.bind((HOST, PORT)) #bind() is used to associate the socket with a specific network interface and port number
-except socket.error as e: #add exception for error
-    print(str(e)) #print error message
+def main():
+    path_exist()
+    print("Server is Opening")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(ADDR) #bind() is used to associate the socket with a specific network interface and port number
+    except socket.error as e: #add exception for error
+        print(str(e)) #print error message
+    s.listen()
+    print("Wating for Connection, listening via", (HOST, PORT))
     
-print("Wating for Connection, listening via", (HOST, PORT))
-s.listen(5) #listen() enables a server to accept() connections
-start = time.time()
+    while True:
+        conn, addr = s.accept()
+        thread = threading.Thread(target=work_client, args=(conn,addr))
+        thread.start()
+        print(f" {threading.activeCount() - 1}")
+        
+def work_client(conn,addr):
+    print("Connected to",(ADDR))
+    conn.send("Connected to Base Station Server".encode(FORM))
 
-def client_thread(connection):
-    connection.send(str.encode('Base Station Server'))
+    recv = conn.recv(buffer_size).decode()
+    filename, filesize = recv.split(space)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    save_path = os.path.join(SERV_PATH,filename)
+
     bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=256, colour='#00ff00') # 256 = MB
-    with open(filename, "wb") as f:
-        while True:
-            data_read = connection.recv(buffer_size) #reads whatever data the client sends and echoes it back using conn.sendall() (1024 bytes) buffer size
-            reply = 'Server Speak: ' + data_read.decode('utf-8')
+    with open(save_path, "wb") as f:
+        while True: #infinite while loop
+            data_read = conn.recv(buffer_size) #reads whatever data the client sends and echoes it back using conn.sendall() (1024 bytes) buffer size
             if not data_read:
                 break
-            connection.sendall(str.encode(reply))
             f.write(data_read)
             bar.update(len(data_read))
-    connection.close()
+    check_file_transfer(filename, save_path, conn)
+    conn.close()        
 
-while True:        
-    client, addr = s.accept() #accept() blocks and waits for an incoming connection
-    print('Connected to: ', addr[0] + ':' + str(addr[1]))
-    start_new_thread(client_thread, (client, ))
-    Threadnumb += 1
-    print('Thread Number: ' + str(Threadnumb))
-    s.close()
+def path_exist():
+    Exist = os.path.exists(SERV_PATH)
+    
+    if not Exist:
+        os.makedirs(SERV_PATH)
+        
+def check_file_transfer(filename, save_path, conn):
+    file = os.path.exists(save_path)
+    if file:
+        print(filename)
+        conn.send(filename.encode(FORM))
+    else:
+        conn.send("File not transfered.".encode(FORM))
 
-end = time.time()
-time = end-start
-xfr_rate = filesize / time
-
-print('\n\nTime = {:.4}s \nFile size = {:.8} MB \nTransfer rate = {:.4} MB/s\n'.format(time, filesize/(1024*1024), xfr_rate/(1024*1024)))
+if __name__ == "__main__":
+    main()
