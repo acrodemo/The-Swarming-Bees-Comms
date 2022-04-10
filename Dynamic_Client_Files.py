8243fc630b820d03a2fa17796311f4d8f04bcea6
aos82 <94140638+aos82@users.noreply.github.com>
2022-04-09 21:04:56 -0600

    Added comments to code

--- Dynamic_Client_Files.py
+++ Dynamic_Client_Files.py
@@ -1,43 +1,44 @@
-import socket
-import tqdm
-import os
-import time
-space = "<space>"
-buffer_size = 1024
+import socket #Socket library
+import tqdm # status bar
+import os #os Library
+
+
+space = "<space>" # Adds Space
+buffer_size = 1024 #number of bytes
 HOST = socket.gethostbyname(socket.gethostname())  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-CLIENT_PATH = "SEND_FILES" 
+CLIENT_PATH = "SEND_FILES" #Folder name
 
-def main():
-    path_exist()
-    filename = os.listdir(CLIENT_PATH)[0]
-    print(filename)
-    run_client(filename)
+def main(): # main function
+    path_exist() #run path exist function
+    filename = os.listdir(CLIENT_PATH)[0] #create a list of files in SEND_FILES folder
+    print(filename) #prints file name to verify name of file
+    run_client(filename) #runs client socket function
     
     
-def run_client (filename):
-    filesize = os.path.getsize(filename) 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run_client (filename): #client socket functions 
+    filesize = os.path.getsize(filename) #get size of file being transfered
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #creates socket object of address family .AF_INET and socket type .SOCKET_STREAM
     s.connect((HOST, PORT)) #Connects to the server    
-    s.send(f"{filename}{space}{filesize}".encode())
-    bar = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=256, colour ='#00ff00')
-    with open(filename, "rb") as f:
-        while True:
-            data = f.read(buffer_size)
-            if not data:
+    s.send(f"{filename}{space}{filesize}".encode()) #sends file and file size
+    bar = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=256, colour ='#00ff00')#establishes download bar
+    with open(filename, "rb") as f: # opens a filename in binary format
+        while True: #while loop sends file
+            data = f.read(buffer_size) #reads file in chunks of bytes
+            if not data:# breaks the while loop if nothing is being sent to data variable
                 break
         
-            s.sendall(data)
-            bar.update(len(data))
-    s.close()
+            s.sendall(data) #sends data to server
+            bar.update(len(data)) #updates the download bar as file is sent to server
+    s.close() #closes client connection
             
-def path_exist():
-    Exist = os.path.exists(CLIENT_PATH)
+def path_exist(): #function to check if folder structure exists if it does not it creates the folder 
+    Exist = os.path.exists(CLIENT_PATH)#uses os librar to verify if a folder exists named SEND_FILES
     
-    if not Exist:
+    if not Exist:#if file does not exist, it creates the file using makedirs.
         os.makedirs(CLIENT_PATH)
             
 
-if __name__ == "__main__":
+if __name__ == "__main__":#runs main function
     main()
 
--- multithread_Server_Files.py
+++ multithread_Server_Files.py
@@ -6,65 +6,65 @@ import os.path
 
 
 buffer_size = 1024
-HOST = socket.gethostbyname(socket.gethostname())
+HOST = socket.gethostbyname(socket.gethostname())#the server's hostname or IP address
 PORT = 65432      # Port to listen on (non-privileged ports are > 1023) should be an integer from 1-65535 (0 is reserved).
-FORM = "utf-8"
-SERV_PATH = "RCVD_FILES"
-ADDR = (HOST, PORT)
+FORM = "utf-8" #encoding format
+SERV_PATH = "RCVD_FILES" #name of folder to put recieved files
+ADDR = (HOST, PORT) #combines host name and port
 space = "<space>"
 
 
-def main():
-    path_exist()
-    print("Server is Opening")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main(): # main function
+    path_exist() #runs path exist function
+    print("Server is Opening") #message to indicate server is ready to open
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#creates socket object of address family .AF_INET and socket type .SOCKET_STREAM
     try:
         s.bind(ADDR) #bind() is used to associate the socket with a specific network interface and port number
     except socket.error as e: #add exception for error
         print(str(e)) #print error message
-    s.listen()
+    s.listen()#listening for a client requesting connection
     print("Wating for Connection, listening via", (HOST, PORT))
     
     while True:
-        conn, addr = s.accept()
-        thread = threading.Thread(target=work_client, args=(conn,addr))
-        thread.start()
+        conn, addr = s.accept()#accepts a connection
+        thread = threading.Thread(target=work_client, args=(conn,addr))#creates a thread and runs workclient functions
+        thread.start()#Threading allows multiple clients to connect to server, each client being its own thread.
         print(f" {threading.activeCount() - 1}")
         
-def work_client(conn,addr):
+def work_client(conn,addr):#work client function is used to recieve file sent from client
     print("Connected to",(ADDR))
     conn.send("Connected to Base Station Server".encode(FORM))
 
-    recv = conn.recv(buffer_size).decode()
-    filename, filesize = recv.split(space)
-    filename = os.path.basename(filename)
-    filesize = int(filesize)
-    save_path = os.path.join(SERV_PATH,filename)
+    recv = conn.recv(buffer_size).decode() #recieves filename and size from file and is decoded
+    filename, filesize = recv.split(space) #splits filename and file size info
+    filename = os.path.basename(filename)#returns the basename of the pathname path
+    filesize = int(filesize) #changes filezise to an interger
+    save_path = os.path.join(SERV_PATH,filename)#joins the folder name and file name to save file in particular folder
 
-    bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=256, colour='#00ff00') # 256 = MB
-    with open(save_path, "wb") as f:
+    bar = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=256, colour='#00ff00') # 256 = MB, establishes load bar
+    with open(save_path, "wb") as f: # opens a filename in binary format
         while True: #infinite while loop
             data_read = conn.recv(buffer_size) #reads whatever data the client sends and echoes it back using conn.sendall() (1024 bytes) buffer size
-            if not data_read:
+            if not data_read: # breaks while loop if data is no longer being sent
                 break
-            f.write(data_read)
-            bar.update(len(data_read))
-    check_file_transfer(filename, save_path, conn)
-    conn.close()        
+            f.write(data_read) #downloads file being sent from client
+            bar.update(len(data_read)) #updates bar as data is being sent from client
+    check_file_transfer(filename, save_path, conn)#function to check if file has been sent
+    conn.close()#closes connection for client        
 
-def path_exist():
-    Exist = os.path.exists(SERV_PATH)
+def path_exist():#function to verify if folder exists
+    Exist = os.path.exists(SERV_PATH)#uses os librar to verify if a folder exists named SEND_FILES
     
-    if not Exist:
+    if not Exist:#if file does not exist, it creates the file using makedirs.
         os.makedirs(SERV_PATH)
         
-def check_file_transfer(filename, save_path, conn):
-    file = os.path.exists(save_path)
-    if file:
-        print(filename)
-        conn.send(filename.encode(FORM))
-    else:
-        conn.send("File not transfered.".encode(FORM))
+def check_file_transfer(filename, save_path, conn):#function to check if file has been transfereed
+    file = os.path.exists(save_path)#chek if file exists in folder returns true or false
+    if file: # if true (file is in folder)
+        print(filename)#prints file name
+        conn.send(filename.encode(FORM))#sends messgae back to client with the name of the file
+    else:#if false (file not in folder)
+        conn.send("File not transfered.".encode(FORM))#sends message back to client that file was not transferred
 
-if __name__ == "__main__":
+if __name__ == "__main__":#runs main function
     main()
